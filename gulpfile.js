83515b2305b4110ea22f5fae7ec38cfcee4cc650
MobileWebPro <betopone1021@gmail.com>
2018-10-19 01:12:50 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
-
+//
 var paths = {
   sass: ['./scss/**/*.scss']
 };
